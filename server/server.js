31ba783a58ee88b57c0a4b8f0ca88da56f0eaa2f
Naga Kummaragunta <nkummara@oradev.oraclecorp.com>
2017-12-23 17:19:20 -0800

    setup heroku

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
+const port = process.env.PORT || 3000;
 var app =express();
 app.use(bodyParser.json());
 
-app.listen(3000,() => {
-  console.log('Started on port 3000');
+app.listen(port,() => {
+  console.log(`Started on port ${port}}`);
 });
 
 app.post('/todos',(req,res) => {
@@ -23,5 +24,22 @@ todo.save().then((doc) => {
 });
 });
 
+app.get('/todos/:id',(req,res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id))
+  res.status(404).send('Id is Invalid');
+
+  Todo.findById(id).then((todo) => {
+if(todo)
+  res.status(200).send(todo);
+  else {
+    res.status(404).send();
+  }
+
+}, (e) => {
+res.status(400).send();
+});
+
+});
 
 //newTodo.save().then((doc) => {console.log('Doc Saved',doc)}, (e) => {console.log('Unable to save',e)});
